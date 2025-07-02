114fe7a7eaf366bb5fb37fa733686ad639e7d5c0
Mohamedelwali <mnoaman162@gmail.com>
2025-07-02 21:51:33 +0300

    Update Endpoints

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('projects/', list_all_projects ),
-    path('projects/<int:pk>/', get_project ),
-    path('projects/<int:pk>/delete/', delete_project ),
-    path('projects/create/', create_project),
-    path('projects/<int:pk>/update/', update_campaign ),
-    path('projects/search/', search_campaigns),
+    path('', list_all_projects ),
+    path('create/', create_project),
+    path('<int:pk>/', get_project ),
+    path('<int:pk>/update/', update_campaign ),
+    path('<int:pk>/delete/', delete_project ),
+    path('search/', search_campaigns),
 ]
--- crowdfunding/urls.py
+++ crowdfunding/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('api/', include('campaigns.urls')),
+    path('api/projects/', include('campaigns.urls')),
 
     # path('api/', include('campaigns.urls'))
 ]
